4b62b45c7d1b2cf406234dcb03372e5805bfa986
Evan hameed <evanhameed9966@gmail.com>
2020-06-01 12:00:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Add your code here
 
+<<<<<<< HEAD
 
 let body = document.getElementsByTagName("body")[0];
+=======
+>>>>>>> 56ff11c41b5613e29a30a91045d417d639d43114
 function submitData(name,email){
 
 let data = {
@@ -13,6 +16,7 @@ let configurationObject = {
 
   method : 'POST',
   headers : { 'content-type':'application/json' , 'Accept':'application/json'},
+<<<<<<< HEAD
   body : JSON.stringify(data)
 
 }
@@ -25,6 +29,12 @@ return fetch ('http://localhost:3000/users',configurationObject)
 
 body.innerHTML = `${error.message}`;
 })
+=======
+  body : JSON.stringify(data);
+
+}
+return fetch ('http://localhost:3000/users',configurationObject)
+>>>>>>> 56ff11c41b5613e29a30a91045d417d639d43114
 
 
 
